2f8a0a119201b5de24dba206b66676c784c0ec2f
Duan Zhiyan <dzy0451@gmail.com>
2015-04-22 01:07:33 +0800

    update automation script template, now it actually works

--- data/script.py
+++ data/script.py
@@ -18,16 +18,26 @@ for step in steps:
         driver.delete_all_cookies()
 
         if cookies:
-            driver.add_cookie(cookies)
+            for name, value in cookies.items():
+                driver.add_cookie({'name': name, 'value': value})
 
         driver.get(url)
 
     if form_fields:
+        submit = None
         for name, value in form_fields.items():
             elem = driver.find_element_by_name(name)
-            driver.execute_script('''
-                var elem = arguments[0];
-                var value = arguments[1];
-                elem.value = value;
-            ''', elem, value)
-        elem.submit()
+            input_type = elem.get_attribute('type')
+
+            if input_type in ['text']:
+                submit = elem
+
+            if input_type == 'file':
+                elem.send_keys(value)
+            else:
+                driver.execute_script('''
+                    var elem = arguments[0];
+                    var value = arguments[1];
+                    elem.value = value;
+                ''', elem, value)
+        submit.send_keys(Keys.RETURN)
